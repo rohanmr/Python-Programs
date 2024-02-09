16dae7cfd0a760c6ae212c8de4638f1aa646fb6f
Rohan2104 <maindarkarrohan@yahoo.com>
2024-02-09 22:41:38 +0530

    first commit

---
+++ Array/SelectionSort.py
@@ -0,0 +1,15 @@
+arr=[24,17,66,45,33,22]
+
+def selectionSort(arr):
+    n=len(arr)
+    for i in range(n-1):
+        mini=i
+        for j in range(i+1,n):
+            if arr[j]<arr[mini]:
+                mini=j
+
+            if mini !=i:
+                arr[i],arr[mini]=arr[mini],arr[i]
+
+selectionSort(arr)
+print(arr)
