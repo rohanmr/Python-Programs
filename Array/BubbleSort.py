b181815fcd0cfae43606da2b021323a35628863f
Rohan2104 <maindarkarrohan@yahoo.com>
2024-02-14 00:07:36 +0530

    Bubble Sort Program

---
+++ Array/BubbleSort.py
@@ -0,0 +1,14 @@
+arr=[50,10,20,40,60,30]
+def bubbleSort(arr):
+    n=len(arr)
+    for i in range(n):
+        swapped=False
+        for j in range(0,n-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+                swapped=True
+        if not swapped:
+            break
+
+bubbleSort(arr)
+print(arr)
