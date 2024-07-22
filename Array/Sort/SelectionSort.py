f4dda9f64cfb527f8468ec805bde96d6e2251ca8
Rohan Maindarkar <122428641+rohanmr@users.noreply.github.com>
2024-07-22 17:28:50 +0530

    added selection sort program

--- Array/Sort/SelectionSort.py
+++ Array/Sort/SelectionSort.py
@@ -1,16 +1,14 @@
-arr=[24,17,66,45,33,22]
+arr=[24,17,55,75,35,88,45,22,2]
 
 def selectionSort(arr):
     n=len(arr)
+
     for i in range(n-1):
         mini=i
         for j in range(i+1,n):
-            if arr[j]<arr[mini]:
+            if arr[j]< arr[mini]:
                 mini=j
-            
-            if mini!=i:
-                arr[i],arr[mini]=arr[mini],arr[i]
+        arr[i],arr[mini]=arr[mini],arr[i]
 
 selectionSort(arr)
-
 print(arr)
