11ec9aad99ff6d420fa722476c8869171e6bf9c8
Rohan Maindarkar <122428641+rohanmr@users.noreply.github.com>
2024-07-24 13:41:35 +0530

    added marg sort code

--- Array/Sort/MergeSort.py
+++ Array/Sort/MergeSort.py
@@ -1,35 +1,35 @@
+arr=[12,55,6,1,2,77,44,66,20,10,78,4,3,99,5]
 
-arr=[12,55,6,1,2,77,44,66,20,10,78,4,3,99]
-
-def merge_sort(arr):
+def margSort(arr):
     if len(arr)<=1:
         return arr
-    mid=len(arr)//2
+    
+    mid= len(arr)//2
+    l_half=arr[:mid]
+    r_half=arr[mid:]
 
-    left_half=arr[:mid]
-    right_half=arr[mid:]
-    left_half=merge_sort(left_half)
-    right_half=merge_sort(right_half)
-    return merge(left_half,right_half)
+    l_half=margSort(l_half)
+    r_half=margSort(r_half)
+    
+    return marge(l_half,r_half)
 
-def merge(left,right):
+def marge(left,right):
     new=[]
     i,j=0,0
-
-    while i < len(left)  and j < len(right):
+    while i < len(left) and j < len(right):
         if left[i]<right[j]:
             new.append(left[i])
             i+=1
         else:
             new.append(right[j])
             j+=1
+    
     new.extend(left[i:])
     new.extend(right[j:])
+
     return new
 
-
-
-sorted_arr=merge_sort(arr)
+sorted_arr=margSort(arr)
 print(sorted_arr)
 
 
