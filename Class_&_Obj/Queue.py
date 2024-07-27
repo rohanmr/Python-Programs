5def97555662b5f463404bac35e9252a6a5431d4
Rohan Maindarkar <122428641+rohanmr@users.noreply.github.com>
2024-07-27 15:49:47 +0530

    added queue program

--- Class_&_Obj/Queue.py
+++ Class_&_Obj/Queue.py
@@ -2,22 +2,27 @@
 class queue:
     def __init__(self) -> None:
         self.values=[]
+    
     def enqueue(self,x):
         self.values.append(x)
+
     def dequeue(self):
-        front=self.values[0]
+        front = self.values[0]
         self.values=self.values[1:]
         return front
 
 
 q=queue()
+
 q.enqueue(10)
 q.enqueue(20)
 q.enqueue(30)
 q.enqueue(40)
 q.enqueue(50)
+q.enqueue(60)
 
-print(q.values)
+print("Queue element is:",q.values)
 
-print(q.dequeue())
-print(q.values)
+print("Dequeue element is :",q.dequeue())
+
+print("New queue list :",q.values)
