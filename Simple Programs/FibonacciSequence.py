f88b1d75bf2480bd9b3f552bde1c5c7bda2d9a8a
Rohan Maindarkar <122428641+rohanmr@users.noreply.github.com>
2024-07-29 15:42:49 +0530

    added two programs

---
+++ Simple Programs/FibonacciSequence.py
@@ -0,0 +1,19 @@
+# Fibonacci Sequence
+
+a=0
+b=1
+
+num= int(input("Enter the no to obtain the sequence: "))
+
+if num == 1:
+    print(a)
+else:
+    print(a)
+    print(b)
+
+    for i in range(2,num):
+        c = a + b
+        a = b
+        b = c 
+
+        print(c)
---
+++ Simple Programs/Swaping.py
@@ -0,0 +1,12 @@
+
+num1=int(input("Enter the no1:"))
+num2=int(input("Enter the no2:"))
+
+
+print("Before swapping:",num1)
+print("Before swapping:",num2)
+
+num1,num2=num2,num1
+
+print("After swapping",num1)
+print("After Swapping",num2)
