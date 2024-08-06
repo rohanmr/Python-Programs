ee295d94d84f72470af10f9ca47090c1206e1a3c
Rohan Maindarkar <122428641+rohanmr@users.noreply.github.com>
2024-08-06 12:14:27 +0530

    added new programs

---
+++ Simple Programs/FindTheMultiplicationOfElement.py
@@ -0,0 +1,9 @@
+#Multiply all elements of the list
+
+myList=[2,4,3,5]
+
+result=1
+for i in myList:
+    result=result*i
+
+print(result)
---
+++ Simple Programs/ReversAndSplitStr.py
@@ -0,0 +1,16 @@
+
+wordStr="Python programming is esy to learn"
+
+print(wordStr)
+
+result=wordStr.split(" ")
+
+print(result)
+
+reversedStr=result[::-1]
+
+print(reversedStr)
+
+outputStr=" ".join(reversedStr)
+
+print(outputStr)
