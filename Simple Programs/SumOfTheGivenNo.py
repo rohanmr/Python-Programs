0107b11d4b0f1b265e2c42934158b94de3c0a735
Rohan Maindarkar <122428641+rohanmr@users.noreply.github.com>
2024-08-14 17:49:32 +0530

    added sum of no program

---
+++ Simple Programs/SumOfTheGivenNo.py
@@ -0,0 +1,14 @@
+# Sum of the Digit of a No (Ex – 123 = 1+2+3 = 6)
+
+n=int(input("Enter the no: "))
+
+result=0
+
+while n > 0:
+    digit= n % 10
+
+    result=result+digit
+
+    n=n//10
+
+print("your sum is:",result)
