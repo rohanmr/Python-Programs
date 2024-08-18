3d7f7718c762bbb6d63265fd78c9385f8c8b40bf
Rohan Maindarkar <122428641+rohanmr@users.noreply.github.com>
2024-08-18 23:17:25 +0530

    added new program

--- Simple Programs/PalindromeOrNot.py
+++ Simple Programs/PalindromeOrNot.py
@@ -1,10 +1,12 @@
 
-string=input("Enter the string:")
+string=input("Enter the string or number:")
+
+print("Your Entered no is:",string)
 
 revStr=string[::-1]
 
 if revStr == string:
-    print("The string is Palindrome")
+    print("The string or number is Palindrome")
 else:
-    print("The string is not Palindrome")
+    print("The string or number is not Palindrome")
     
