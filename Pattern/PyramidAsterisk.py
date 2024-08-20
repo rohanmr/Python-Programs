68b7f0da1fe2dee636e19755702883d11520ebf0
Rohan Maindarkar <122428641+rohanmr@users.noreply.github.com>
2024-08-20 23:27:21 +0530

    added new pattern program

---
+++ Pattern/PyramidAsterisk.py
@@ -0,0 +1,18 @@
+# Write a  program to make such a pattern as a pyramid with an asterisk.
+#    * 
+#   * * 
+#  * * * 
+# * * * *
+
+
+num=int(input("Enter the no:"))
+n="*"
+
+for i in range(1,num+1):
+    for j in range(i,num):
+        print(" ",end="")
+    
+    for j in range(1,i+1):
+        print(n,end=" ")
+    print()
+
