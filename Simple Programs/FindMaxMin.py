a4b8ea3788e2549529f2e18f628f1d6c3d961c9e
Rohan Maindarkar <122428641+rohanmr@users.noreply.github.com>
2024-08-21 22:21:10 +0530

    max min program

--- Simple Programs/FindMaxMin.py
+++ Simple Programs/FindMaxMin.py
@@ -14,3 +14,22 @@ for i in range(len(list)):
 print("Maximum is",maximum)
 
 print("Minimum is ",minimum)
+
+
+
+# num=[10,50,40,60,90,800,30]
+
+
+# maximum=num[0]
+# minimum=num[0]
+
+# for i in range(len(num)):
+#     if num[i]>maximum:
+#         maximum=num[i]
+#     elif num[i]<minimum:
+#         minimum=num[i]
+
+    
+# print("Maximum is",maximum)
+
+# print("Minimum is ",minimum)
