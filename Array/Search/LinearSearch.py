392d0278007f907797ffe373f70c2c68da0c5271
Rohan Maindarkar <122428641+rohanmr@users.noreply.github.com>
2024-08-22 22:56:56 +0530

    added new program

--- Array/Search/LinearSearch.py
+++ Array/Search/LinearSearch.py
@@ -1,6 +1,7 @@
+# Search a no by using linear search .
 
 arr=[10,50,70,90,60,30,20,40]
-target=90
+target=int(input("Enter the no: "))
 
 def LinearSearch(arr,target):
     for i in range(len(arr)):
@@ -9,3 +10,25 @@ def LinearSearch(arr,target):
         
 result=LinearSearch(arr,target)
 print("This is your Search element",result)
+
+
+
+
+# arr=[20,40,50,70,90,60,30]
+
+# target=int(input("Enter the no:"))
+
+# def linearSearch(arr,target):
+#     for i in range(len(arr)):
+#         if arr[i]==target:
+#             return arr[i]
+#     else:
+#         print("No is not in the array")
+        
+
+
+
+# result=linearSearch(arr,target)
+
+
+# print("Your searched no is",result)
