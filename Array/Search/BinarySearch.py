f903d187d59ca4ae1d833f0a2259d9c665e0f7c9
Rohan Maindarkar <122428641+rohanmr@users.noreply.github.com>
2024-08-23 23:50:00 +0530

    added new program

--- Array/Search/BinarySearch.py
+++ Array/Search/BinarySearch.py
@@ -1,19 +1,42 @@
 
+# arr=[10,20,30,40,50,60,70]
+# target=30
+
+# def binarySearch(arr,target):
+#     left,right=0, len(arr)-1
+#     while left<=right:
+#         mid=(left+right)//2
+
+#         if(arr[mid]==target):
+#             return arr[mid]
+#         elif(arr[mid]<target):
+#             left=mid+1
+#         else:
+#             right=mid-1
+
+# result=binarySearch(arr,target)
+
+# print(result)
+
+
+
 arr=[10,20,30,40,50,60,70]
-target=30
+target=int(input("enter the no like 10,20,30...: "))
 
-def binarySearch(arr,target):
-    left,right=0, len(arr)-1
-    while left<=right:
-        mid=(left+right)//2
+def binarySearch(arr, target):
+    left, right=0,len(arr)-1
 
-        if(arr[mid]==target):
+    while left <= right:
+        mid = (left +  right) // 2
+
+        if arr[mid] == target:
             return arr[mid]
-        elif(arr[mid]<target):
-            left=mid+1
+        elif arr[mid] < target:
+            left= mid+1
         else:
             right=mid-1
 
-result=binarySearch(arr,target)
+
+result=binarySearch(arr, target)
 
 print(result)
