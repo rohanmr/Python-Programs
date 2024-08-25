dc4582009f375174cd0c91453c97750ad48fb06d
Rohan Maindarkar <122428641+rohanmr@users.noreply.github.com>
2024-08-25 23:37:42 +0530

    added insertion sort program

--- Array/Sort/InsertionSort.py
+++ Array/Sort/InsertionSort.py
@@ -1,11 +1,15 @@
-arr=[90,10,30,50,70,40,80,60,20]
+# Sort the array using insertion sort.
+
+arr=[40,10,50,60,80,90,30,70,20]
 
 def insertionSort(arr):
     n=len(arr)
+
     for i in range(1,n):
         key=arr[i]
         j=i-1
-        while j>=0 and key<arr[j]:
+
+        while j>=0 and key < arr[j]:
             arr[j+1]=arr[j]
             j-=1
         arr[j+1]=key
