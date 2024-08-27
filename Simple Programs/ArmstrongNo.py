4f9cd5a364f799fa8bd9ea5594a6fe25804dd50b
Rohan Maindarkar <122428641+rohanmr@users.noreply.github.com>
2024-08-27 23:27:14 +0530

    added new program

--- Simple Programs/ArmstrongNo.py
+++ Simple Programs/ArmstrongNo.py
@@ -1,3 +1,4 @@
+# Check if a given No is Armstrong No or not ????
 num=input("Enter the Number:")
 
 sum=0
@@ -9,3 +10,18 @@ if sum == int(num):
     print("Number is Armstrong")
 else:
     print("Number is not Armstrong")
+
+
+# num=input("Enter the no: ")
+
+# def CheckNO(num):
+#     sum=0
+#     for i in num:
+#         sum+=int(i)**3
+        
+#     if sum == int(num):
+#         print("no is Armstrong")
+#     else:
+#         print("no is not Armstrong")
+        
+# CheckNO(num)
