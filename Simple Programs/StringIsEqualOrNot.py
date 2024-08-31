b2378250ee844899a75922ed98d001dbcee606b1
Rohan Maindarkar <122428641+rohanmr@users.noreply.github.com>
2024-08-31 23:11:34 +0530

    added new program

---
+++ Simple Programs/StringIsEqualOrNot.py
@@ -0,0 +1,10 @@
+# Check if two strings are equal or not? By being case  sensitive?
+
+str1=input("Enter the words: ")
+str2=input("Enter the another word:")
+
+if str1.lower() ==  str2.lower():
+    print("String is equal")
+else:
+    print("String is not equal")
+    
