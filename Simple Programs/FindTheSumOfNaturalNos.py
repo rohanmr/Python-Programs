2053214fe6bac731888fbbbffc803bb5dd9b7fa4
Rohan Maindarkar <122428641+rohanmr@users.noreply.github.com>
2024-09-30 21:52:23 +0530

    added new changes

--- Simple Programs/FindTheSumOfNaturalNos.py
+++ Simple Programs/FindTheSumOfNaturalNos.py
@@ -1,11 +1,11 @@
-num =int(input("Enter the no:"))
+num=int(input("Enter the number to obtain the sum:"))
 
 sum=0
 
 while num > 0:
     for i in range(1,num+1):
-        sum=sum+i
-    
-    num=-1
+        sum=sum+1
 
-print("Sum of the natural numbers are:",sum)
+    num-=1
+    
+print("The obtained sum is:",sum)
