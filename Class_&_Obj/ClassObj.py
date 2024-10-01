849b974702deca5dbedf5df271b2b36209a50551
Rohan Maindarkar <122428641+rohanmr@users.noreply.github.com>
2024-10-01 22:50:50 +0530

    added some fixes

--- Class_&_Obj/ClassObj.py
+++ Class_&_Obj/ClassObj.py
@@ -1,17 +1,17 @@
 
 class employee:
-    def putdata(self):
+    def PutData(self):
         self.id=int(input("Enter Employee Id:"))
         self.name=input("Enter Employee Name: ")
-        self.salery=float(input("Enter Employee Salery:"))
+        self.salary=float(input("Enter Employee Salary:"))
 
     def display(self):
         print("|---------------------------------|")
         print("Employee Id is:",self.id)
         print("Employee Name is:",self.name)
-        print("Employee Salery is:",self.salery)
+        print("Employee Salary is:",self.salary)
         print("|----------------------------------|")
 
 a=employee()
-a.putdata()
+a.PutData()
 a.display()
--- Class_&_Obj/Cunstructor.py
+++ Class_&_Obj/Cunstructor.py
@@ -2,13 +2,13 @@ class employee:
     def __init__(self) -> None:
         self.id=int(input("Enter Employee Id:"))
         self.name=input("Enter Employee Name: ")
-        self.salery=float(input("Enter Employee Salery:"))
+        self.salary=float(input("Enter Employee Salary:"))
 
     def display(self):
         print("|---------------------------------|")
         print("Employee Id is:",self.id)
         print("Employee Name is:",self.name)
-        print("Employee Salery is:",self.salery)
+        print("Employee Salary is:",self.salary)
         print("|----------------------------------|")
 
 a=employee()
