aa5634eab74e5cc2ed1dca2dfb72107399b0cb96
Rohan Maindarkar <122428641+rohanmr@users.noreply.github.com>
2024-10-02 22:56:12 +0530

    added some new changes

--- Array/Sort/InsertionSort.py
+++ Array/Sort/InsertionSort.py
@@ -12,7 +12,9 @@ def insertionSort(arr):
         while j>=0 and key < arr[j]:
             arr[j+1]=arr[j]
             j-=1
+        
         arr[j+1]=key
 
 insertionSort(arr)
+
 print(arr)
--- Array/Sort/SelectionSort.py
+++ Array/Sort/SelectionSort.py
@@ -7,13 +7,14 @@ def selectionSort(arr):
 
     for i in range(n-1):
         mini=i
-
         for j in range(i+1,n):
             if arr[j]<arr[mini]:
-                mini=j 
-        arr[i],arr[mini]= arr[mini],arr[i]
-
+                mini=j
+            
+        
+        arr[i],arr[mini]=arr[mini],arr[i]
 
 selectionSort(arr)
-
 print(arr)
+
+
