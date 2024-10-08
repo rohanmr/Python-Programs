7ab15c7ea4ac82eecfd7bfed48fc0a8869ab60ed
Rohan Maindarkar <122428641+rohanmr@users.noreply.github.com>
2024-10-08 23:11:55 +0530

    added new changes

--- Simple Programs/FindNoIsPerfectOrNot.py
+++ Simple Programs/FindNoIsPerfectOrNot.py
@@ -1,34 +1,38 @@
 # It's a positive integer the sum of all its factors is equal to the number the this is a perfect number
-num=int(input("Enter the number: "))
+num=int(input("Enter the no :"))
 
-def perfectNo(num):
-    sum=0
-    for i in range(1,num):
-        if num % i == 0:
-            sum=sum+i
+sum=0
+
+for i in range(1,num):
+    if num % i == 0:
+        sum=sum+i
+
+
+if sum == num:
+    print("Entered number is perfect number")
+else:
+    print("Entered number is not perfect")
+
+
+
+
+
+
+# num=int(input("Enter the number: "))
+
+# def perfectNo(num):
+#     sum=0
+#     for i in range(1,num):
+#         if num % i == 0:
+#             sum=sum+i
     
-    if sum == num:
-        print("Entered no is perfect")
-    else:
-        print("enter no is not a perfect no")
+#     if sum == num:
+#         print("Entered no is perfect")
+#     else:
+#         print("enter no is not a perfect no")
 
-perfectNo(num)
+# perfectNo(num)
 
-
-
-
-
-
-# sum=0
-
-# for i in range(1,num):
-#     if num % i ==0:
-#         sum+=i
-
-# if sum == num:
-#     print("Number is perfect no")
-# else:
-#     print("Number is not perfect no")
 
 
 
