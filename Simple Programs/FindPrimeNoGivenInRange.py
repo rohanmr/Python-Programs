ae748fa8461fe1b3ebedb16bfe8ef4df764b727d
Rohan Maindarkar <122428641+rohanmr@users.noreply.github.com>
2024-10-09 22:45:15 +0530

    added  some code changes

--- Simple Programs/FindPrimeNoGivenInRange.py
+++ Simple Programs/FindPrimeNoGivenInRange.py
@@ -1,10 +1,11 @@
 # Find Prime No in a given ranges ( ex prime no b/w 1 to 20 : 2,3,5,7,11,13,17,19)
 
-num =int(input("enter the no: "))
+num=int(input("Enter the number: "))
 
-for i in range(2,num+1):
+for i in range(2, num+1):
     for j in range(2,i):
-        if (i % j)==0:
+        if(i % j) == 0:
             break
     else:
         print(i)
+
