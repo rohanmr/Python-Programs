e9935f34eb525ac6eedecede44d2213847f3308b
Rohan Maindarkar <122428641+rohanmr@users.noreply.github.com>
2024-10-10 17:47:02 +0530

    added new changes

--- Simple Programs/FindFirstNonRepetatingLetter.py
+++ Simple Programs/FindFirstNonRepetatingLetter.py
@@ -2,15 +2,12 @@
 
 string=input("Enter the the string: ")
 
-def first_non_repeating_char(string):
-    
+def first_non_repeating_string(string):
     for i in range(len(string)):
         if string.count(string[i])==1:
             return string[i]
-            
-    
     return None
-    
-    
-res=first_non_repeating_char(string)
-print(res)
+
+
+result=first_non_repeating_string(string)
+print(result)
