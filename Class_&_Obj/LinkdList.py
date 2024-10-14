6939fb6a9f8a7e8808862a482918744a8342309a
Rohan Maindarkar <122428641+rohanmr@users.noreply.github.com>
2024-10-14 21:59:31 +0530

    added new changes

--- Class_&_Obj/LinkdList.py
+++ Class_&_Obj/LinkdList.py
@@ -1,8 +1,11 @@
+# LinkList Program 
+
 
 class Node:
     def __init__(self,data) -> None:
         self.data=data
         self.next=None
+
 
 node1=Node(10)
 node2=Node(20)
@@ -13,9 +16,11 @@ node1.next=node2
 node2.next=node3
 node3.next=node4
 
-current =node1
+current=node1
 
 while current is not None:
-    print(current.data,end="-->")
+    print(current.data,end=" --> ")
+
     current=current.next
+
 print("None")
