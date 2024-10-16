c2964cf1d68d930af4eea800d3e2c383099642b7
Rohan Maindarkar <122428641+rohanmr@users.noreply.github.com>
2024-10-16 20:42:01 +0530

    added some new changes

--- Simple Programs/FindTheSumOfElement.py
+++ Simple Programs/FindTheSumOfElement.py
@@ -1,12 +1,28 @@
 # find the sum of all element of list 
 
-myList=[20,40,80,90,30,70,60,10]
+myList=[20,40,80,90,30,70,60,10,50]
 
-n=len(myList)
+# n=len(myList)
 
-total=0
+# total=0
 
-for i in range(0,n):
-    total=total+myList[i]
+# for i in range(0,n):
+#     total=total+myList[i]
 
-print("Sum of list elements are: ",total)
+# print("Sum of list elements are: ",total)
+
+
+def addArrayOfNumbers(myList):
+    n=len(myList)
+    sum=0
+
+    for i in range(0,n):
+        sum=sum+myList[i]
+
+    return sum
+
+result=addArrayOfNumbers(myList)
+
+print(result)
+
+    
