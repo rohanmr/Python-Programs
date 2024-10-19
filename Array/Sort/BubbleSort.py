a111dd635171781f4f01660750e1e2386878f25c
Rohan Maindarkar <122428641+rohanmr@users.noreply.github.com>
2024-10-19 15:59:40 +0530

    added new changes

--- Array/Sort/BubbleSort.py
+++ Array/Sort/BubbleSort.py
@@ -1,23 +1,22 @@
 # Sort the array using Bubble sort.
 
-arr=[20,60,80,70,10,90,50,40,30,1,4,3,2,5]
-
-
+arr=[20,60,80,70,10,90,50,40,30,1,4,3,2,5,0]
 
 def bubbleSort(arr):
     n=len(arr)
-    for  i in range(n-1):
+    for i in range(n-1):
         swapped=False
         for j in range(0,n-i-1):
-            if arr[j]>arr[j+1]:
-                arr[j],arr[j+1]=arr[j+1],arr[j]
-                swapped=True
-
+           if arr[j] > arr[j+1]:
+               arr[j+1],arr[j]=arr[j],arr[j+1]
+               swapped=True
+        
         if not swapped:
             break
+    
     return arr
 
 
 res=bubbleSort(arr)
 
-print(res)
+print("Your Sorted array is:",res)
