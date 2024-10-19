1dfa05b2bca5ef9dcaf191744569596594b261fe
Rohan Maindarkar <122428641+rohanmr@users.noreply.github.com>
2024-10-19 15:48:10 +0530

    added new changes

--- Array/Sort/InsertionSort.py
+++ Array/Sort/InsertionSort.py
@@ -3,17 +3,17 @@
 arr=[40,10,50,60,80,90,30,70,20]
 
 def insertionSort(arr):
+    #lets count length of array
     n=len(arr)
-
+    #For loop to traverse the array 
     for i in range(1,n):
         key=arr[i]
         j=i-1
-
-        while j>=0 and key < arr[j]:
+        while j >= 0 and key < arr[j]:
             arr[j+1]=arr[j]
-            j-=1
-        
-        arr[j+1]=key
+            j-=1 
+
+        arr[j+1]=key            
 
 insertionSort(arr)
 
