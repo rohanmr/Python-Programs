10a57226bd5d91b2ac37afd9b458107e8d3408e0
Rohan Maindarkar <122428641+rohanmr@users.noreply.github.com>
2024-10-20 22:07:49 +0530

    added some changes

--- Array/Sort/SelectionSort.py
+++ Array/Sort/SelectionSort.py
@@ -1,20 +1,23 @@
 # Sort the array using Selection sort.
 
-arr=[20,8,9,1,90,60,10,2,55,3,0]
+arr=[20,8,9,1,90,60,10,2,55,3,0,4]
+
+#Function to sort array by using selectionSort
 
 def selectionSort(arr):
+    # Calculate the length of array
     n=len(arr)
 
-    for i in range(n-1):
+    for i in range(n):
         mini=i
         for j in range(i+1,n):
             if arr[j]<arr[mini]:
                 mini=j
-            
         
-        arr[i],arr[mini]=arr[mini],arr[i]
+        # Swapping the element of array
+        arr[mini],arr[i]=arr[i],arr[mini]
+
 
 selectionSort(arr)
-print(arr)
 
-
+print("Your Sorted array is:",arr)
