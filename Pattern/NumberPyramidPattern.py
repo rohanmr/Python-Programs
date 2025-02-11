64efeda42591c7fa510c4dd02cbdc44fb9eb0fbc
Rohan Maindarkar <122428641+rohanmr@users.noreply.github.com>
2025-02-11 19:30:08 +0530

    added new code

--- Pattern/NumberPyramidPattern.py
+++ Pattern/NumberPyramidPattern.py
@@ -4,14 +4,16 @@
 #  4 5 6 
 # 7 8 9 10 
 
-num=4
-n=1
+n=5
+num=1
 
-for i in range(1,num+1):
-    for j in range(num-i):
+for i in range(n-1):
+    for j in range(i,n):
         print(" ",end="")
     
-    for j in range(1,i+1):
-        print(n,end=" ")
-        n=n+1
+    for j in range(i+1):
+        print(num,end=" ")
+        num=num+1
+    
     print()
+
