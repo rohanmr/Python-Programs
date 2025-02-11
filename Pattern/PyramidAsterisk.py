8d1503d6411e15bf656aa4426b3a4b4772fcf17a
Rohan Maindarkar <122428641+rohanmr@users.noreply.github.com>
2025-02-11 19:59:19 +0530

    new changes

--- Pattern/PyramidAsterisk.py
+++ Pattern/PyramidAsterisk.py
@@ -8,9 +8,24 @@
 num=int(input("Enter the no:"))
 n="*"
 
-for i in range(1,num+1):
+for i in range(num-1):
     for j in range(i,num):
         print(" ",end="")
-    for j in range(1,i+1):
+    for j in range(i+1):
         print(n,end=" ")
+    
     print()
+
+
+
+
+
+
+# for i in range(1,num+1):
+#     for j in range(i,num):
+#         print(" ",end="")
+#     for j in range(1,i+1):
+#         print(n,end=" ")
+#     print()
+
+
